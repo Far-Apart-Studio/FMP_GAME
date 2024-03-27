6919cc0a63d8adf5be70f3cca0c1a6d434c23e6c
DENraynaH <103390897+DENraynaH@users.noreply.github.com>
2024-03-27 11:36:06 +0000

    throwing temp fixes till animation

---
+++ Source/PROJECT_WEST/PW_GrenadeObject.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PW_ItemObject.h"
+#include "PW_GrenadeObject.generated.h"
+
+UCLASS()
+class PROJECT_WEST_API APW_GrenadeObject : public APW_ItemObject
+{
+	GENERATED_BODY()
+
+	
+	
+};
