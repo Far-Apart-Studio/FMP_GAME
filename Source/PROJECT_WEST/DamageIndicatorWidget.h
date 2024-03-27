3463c6942d1a2b0b09f40a4c443115340625f0a7
DENraynaH <103390897+DENraynaH@users.noreply.github.com>
2024-03-27 00:01:10 +0000

    damage indicator finished

--- Source/PROJECT_WEST/DamageIndicatorWidget.h
+++ Source/PROJECT_WEST/DamageIndicatorWidget.h
@@ -6,12 +6,43 @@
 #include "Blueprint/UserWidget.h"
 #include "DamageIndicatorWidget.generated.h"
 
-/**
- * 
- */
+USTRUCT(BlueprintType)
+struct FIndicateHandle
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) AActor* ownerActor;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) AActor* damageCauser;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) float damageAmount;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) float duration;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) float renderThreshold;
+};
+
 UCLASS()
 class PROJECT_WEST_API UDamageIndicatorWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+private:
+	UPROPERTY() FIndicateHandle _indicateHandle;
+
+public:
+	UFUNCTION(BlueprintCallable) void SetHitDetails(FIndicateHandle hitDetails) { _indicateHandle = hitDetails; }
+	UFUNCTION(BlueprintCallable) void SelfDestruct();
+	UFUNCTION(BlueprintCallable) float GetClampedAngle();
+	UFUNCTION(BlueprintCallable) float GetDotProductBetweenEnemy();
+	UFUNCTION(BlueprintCallable) float GetCrossProductBetweenEnemy();
+	UFUNCTION(BlueprintCallable) void GetDotAndCrossProductBetweenEnemy(float& dotProduct, float& crossProduct);
+
 	
+	UFUNCTION(BlueprintCallable, Category = "Damage Indicator")
+
+	/**
+	* @brief
+	* Validates the render criteria for the widget.
+	* When the dot product difference is greater than the render threshold,
+	* The widgets opacity is set to 0.0f, otherwise its set to 1.0f.
+	*/
+	
+	void ValidateRenderStatus();
 };
